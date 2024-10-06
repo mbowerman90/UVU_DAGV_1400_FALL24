9fceb933e3f8a9961e7c758373b010e4050da6cd
mbowerman90 <marshallbowerman@gmail.com>
2024-10-05 20:47:27 -0600

    Lab 5 uploads
    Project is now called "2d Platformer Learning", including scripts and SampleScene as well as screenshot of Animator tab

---
+++ Lab 5/SimpleCharacterController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharacterController controller;
+    private Transform thisTransform;
+    private Vector3 movementVector = Vector3.zero;
+    
+    private void Start()
+    {
+        //Cache references to components
+        controller = GetComponent<CharacterController>();
+        thisTransform = transform;
+    }
+    
+    private void Update()
+    {
+        MoveCharacter();
+        KeepCharacterOnXAxis();
+    }
+    
+    private void MoveCharacter()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector *= (moveSpeed * Time.deltaTime);
+        controller.Move(movementVector);
+    }
+    
+    private void KeepCharacterOnXAxis()
+    {
+        //Use cached transform reference and optimize vector creation
+        var currentPosition = thisTransform.position;
+        currentPosition.z = 0f;
+        thisTransform.position = currentPosition;
+    }
+}
