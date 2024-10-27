212537c53c36bd750b8b601ac44f2e0b6590d254
mbowerman90 <marshallbowerman@gmail.com>
2024-10-27 15:37:59 -0600

    Challenge 7 commit
    Got the second UI image and data type added in, cleaned up where I had them appear on screen

---
+++ Challenge 7/Scripts/SimpleImageBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SimpleImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    public SimpleFloatData dataObj;
+
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+
+    public void UpdateWithFloatData()
+    {
+        imageObj.fillAmount = dataObj.value;
+    }
+}
